294899b86d7042a36ff1e227396535afd5d60789
LinkBoi00 <linkdevel@protonmail.com>
2023-01-01 21:59:25 +0200

    drivers: misc/mtk: apusys: Tone down debug and verbose logging
    [bengris32]: Apply changes to other apusys drivers
    
    Change-Id: I083b9d8b0b30f5df81dab5cc21c606db0d6c6251
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- drivers/misc/mediatek/apusys/debug/debug_driver.c
+++ drivers/misc/mediatek/apusys/debug/debug_driver.c
@@ -304,11 +304,6 @@ static int debug_probe(struct platform_device *pdev)
 	}
 
 	pr_notice("debug probe done, dbglog_buf= 0x%p\n", dbglog_buf);
-	ret = apusys_dump_init(&pdev->dev);
-	if (ret) {
-		DBG_LOG_ERR("failed to create debug dir.\n");
-		goto out;
-	}
 
 	/* create device table info */
 	apusys_debug_devinfo = debugfs_create_file("log", 0444,
@@ -338,16 +333,8 @@ out:
 	return ret;
 }
 
-static int debug_remove(struct platform_device *pdev)
-{
-	apusys_dump_exit(&pdev->dev);
-	return 0;
-}
-
-
 static struct platform_driver debug_driver = {
 	.probe = debug_probe,
-	.remove = debug_remove,
 	.driver = {
 		   .name = APUDBG_DEV_NAME,
 		   .owner = THIS_MODULE,
