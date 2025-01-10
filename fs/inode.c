e97d27dab4b3ebedc575cb17e454dd9b31954874
wulan17 <wulan17@nusantararom.org>
2025-01-10 22:03:07 +0700

    Revert "gale: Import susfs tag `2bf045bbf84c8b506dfaf749bbe9226d8dc4de06`"
    This reverts commit 3a6df0244f727d0b18c712b3c1a346f0983b0a9c.
    
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- fs/inode.c
+++ fs/inode.c
@@ -22,10 +22,6 @@
 #include <linux/iversion.h>
 #include <trace/events/writeback.h>
 #include "internal.h"
- 
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-extern bool susfs_is_current_ksu_domain(void);
-#endif
 
 /*
  * Inode locking rules:
@@ -1645,12 +1641,6 @@ int generic_update_time(struct inode *inode, struct timespec64 *time, int flags)
 {
 	int iflags = I_DIRTY_TIME;
 	bool dirty = false;
- 
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-	if (susfs_is_current_ksu_domain()) {
-		return 0;
-	}
-#endif
 
 	if (flags & S_ATIME)
 		inode->i_atime = *time;
@@ -1678,12 +1668,6 @@ EXPORT_SYMBOL(generic_update_time);
 static int update_time(struct inode *inode, struct timespec64 *time, int flags)
 {
 	int (*update_time)(struct inode *, struct timespec64 *, int);
- 
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-	if (susfs_is_current_ksu_domain()) {
-		return 0;
-	}
-#endif
 
 	update_time = inode->i_op->update_time ? inode->i_op->update_time :
 		generic_update_time;
@@ -1740,12 +1724,6 @@ void touch_atime(const struct path *path)
 	struct vfsmount *mnt = path->mnt;
 	struct inode *inode = d_inode(path->dentry);
 	struct timespec64 now;
- 
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-	if (susfs_is_current_ksu_domain()) {
-		return;
-	}
-#endif
 
 	if (!atime_needs_update(path, inode))
 		return;
