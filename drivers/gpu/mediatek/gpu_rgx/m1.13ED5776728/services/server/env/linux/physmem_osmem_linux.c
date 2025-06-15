b6334af368e4c48475cfdeb8a892ed4f690f56da
wulan17 <wulan17@komodos.id>
2025-06-15 14:45:45 +0700

    gale: drivers: gpu: Import Xiaomi changes
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- drivers/gpu/mediatek/gpu_rgx/m1.13ED5776728/services/server/env/linux/physmem_osmem_linux.c
+++ drivers/gpu/mediatek/gpu_rgx/m1.13ED5776728/services/server/env/linux/physmem_osmem_linux.c
@@ -3377,13 +3377,6 @@ e0:
 	return eError;
 }
 
-static INLINE IMG_BOOL IsOffsetValid(const PMR_OSPAGEARRAY_DATA *psOSPageArrayData,
-                                     IMG_UINT32 ui32Offset)
-{
-	return (ui32Offset >> psOSPageArrayData->uiLog2AllocPageSize) <
-	    psOSPageArrayData->uiTotalNumOSPages;
-}
-
 /* Determine PA for specified offset into page array. */
 static IMG_DEV_PHYADDR GetOffsetPA(const PMR_OSPAGEARRAY_DATA *psOSPageArrayData,
                                    IMG_UINT32 ui32Offset)
@@ -3393,6 +3386,7 @@ static IMG_DEV_PHYADDR GetOffsetPA(const PMR_OSPAGEARRAY_DATA *psOSPageArrayData
 	IMG_UINT32 ui32InPageOffset = ui32Offset - (ui32PageIndex << ui32Log2AllocPageSize);
 	IMG_DEV_PHYADDR sPA;
 
+	PVR_ASSERT(ui32PageIndex < psOSPageArrayData->uiTotalNumOSPages);
 	PVR_ASSERT(ui32InPageOffset < (1U << ui32Log2AllocPageSize));
 
 	sPA.uiAddr = page_to_phys(psOSPageArrayData->pagearray[ui32PageIndex]);
@@ -3427,9 +3421,6 @@ PMRSysPhysAddrOSMem(PMR_IMPL_PRIVDATA pvPriv,
 	{
 		if (pbValid[uiIdx])
 		{
-			PVR_LOG_RETURN_IF_FALSE(IsOffsetValid(psOSPageArrayData, puiOffset[uiIdx]),
-			                        "puiOffset out of range", PVRSRV_ERROR_OUT_OF_RANGE);
-
 			psDevPAddr[uiIdx] = GetOffsetPA(psOSPageArrayData, puiOffset[uiIdx]);
 
 #if !defined(PVR_LINUX_PHYSMEM_USE_HIGHMEM_ONLY)
--- drivers/gpu/mediatek/gpu_rgx/m1.13ED5776728/services/server/env/linux/pmr_os.c
+++ drivers/gpu/mediatek/gpu_rgx/m1.13ED5776728/services/server/env/linux/pmr_os.c
@@ -396,10 +396,6 @@ OSMMapPMRGeneric(PMR *psPMR, PMR_MMAP_DATA pOSMMapData)
 	IMG_BOOL bUseVMInsertPage = IMG_FALSE;
 	IMG_DEVMEM_SIZE_T uiPmrVirtualSize;
 
-	/* if writeable but not shared mapping is requested then fail */
-	PVR_RETURN_IF_INVALID_PARAM(((ps_vma->vm_flags & VM_WRITE) == 0) ||
-	                            ((ps_vma->vm_flags & VM_SHARED) != 0));
-
 	uiLength = ps_vma->vm_end - ps_vma->vm_start;
 
 	PMR_LogicalSize(psPMR, &uiPmrVirtualSize);
@@ -434,6 +430,13 @@ OSMMapPMRGeneric(PMR *psPMR, PMR_MMAP_DATA pOSMMapData)
 	if (eError != PVRSRV_OK)
 	{
 		goto ErrUnrefPMR;
+	}
+
+	if (((ps_vma->vm_flags & VM_WRITE) != 0) &&
+		((ps_vma->vm_flags & VM_SHARED) == 0))
+	{
+		eError = PVRSRV_ERROR_INVALID_PARAMS;
+		goto e1;
 	}
 
 	/* Increment mapping count of the PMR so that its layout cannot be
